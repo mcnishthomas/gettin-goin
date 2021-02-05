b5c38062b3f64ad1c6c91be41fa3d61a0ad064ab
Thomas McNish <mcnishthomas@gmail.com>
2021-02-04 21:31:02 -0500

    reverted

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const app = Vue.createApp({
     data() {
         return {
 <<<<<<< HEAD
+<<<<<<< HEAD
             courseGoal: 'Finish the course and learn Vue.'
             vueLink: 'https://vuejs.org'
+=======
+            courseGoal: 'Finish the course and learn Vue.';
+>>>>>>> parent of 884b17f... first interpolation
 =======
             courseGoal: 'Finish the course and learn Vue.';
 >>>>>>> parent of 884b17f... first interpolation
