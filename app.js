b56eef46317c8d1fba39a33634d8109967eb74ad
Thomas McNish <mcnishthomas@gmail.com>
2021-02-04 21:30:19 -0500

    updated v-bind

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const app = Vue.createApp({
     data() {
         return {
+<<<<<<< HEAD
             courseGoal: 'Finish the course and learn Vue.'
             vueLink: 'https://vuejs.org'
+=======
+            courseGoal: 'Finish the course and learn Vue.';
+>>>>>>> parent of 884b17f... first interpolation
         };
     }
 });
